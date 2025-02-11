d51c4d16899351d10d3f98beee5bd329e8cabac9
guney <supurjsgml1@naver.com>
2025-02-11 13:11:13 +0900

    레스트 템플릿 > 웹클라이언트 통신으로 변경

--- src/main/java/com/app/job/jobKorea/JobKoreaScheduleler.java
+++ src/main/java/com/app/job/jobKorea/JobKoreaScheduleler.java
@@ -3,19 +3,22 @@ package com.app.job.jobKorea;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.app.job.jobKorea.service.JobKoreaResumeUpdaterService;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class JobKoreaScheduleler {
-    private final JobKoreaResumeUpdaterService updater;
 
-    public JobKoreaScheduleler(JobKoreaResumeUpdaterService updater) {
-        this.updater = updater;
-    }
+	private final JobKoreaResumeUpdaterService updater;
+    
+    private final WebClient webClient;
+
 
     //매일 1시간마다 실행
     @Scheduled(cron = "0 0/30 * * * ?")
@@ -27,11 +30,12 @@ public class JobKoreaScheduleler {
     //죽지마오..
     @Scheduled(cron = "0 0/10 * * * ?")
     public void stilALive() {
-    	try {
-    		RestTemplate rt = new RestTemplate();
-    		rt.getForEntity("https://jcheduler-job-7b7308a3f9fd.herokuapp.com", null);
-		} catch (Exception e) {
-			log.info("저 아직 살아 있어효");
-		}
+    	 webClient.get()
+         .uri("https://jcheduler-job-7b7308a3f9fd.herokuapp.com")
+         .retrieve()
+         .bodyToMono(String.class)
+         .doOnSuccess(response -> log.info("✅ Heroku Keep-Alive 성공"))
+         .doOnError(error -> log.info("저 아직 살아 있어효 : {}", error.getMessage()))
+         .subscribe();
     }
 }
