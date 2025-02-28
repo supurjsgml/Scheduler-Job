c30b228b9ba0bb4bad40749fe662a55ab41fc488
guney <supurjsgml1@naver.com>
2025-02-28 16:25:45 +0900

    크롬 드라이버 호출 최적화

--- src/main/java/com/app/job/jobKorea/service/JobKoreaResumeUpdaterService.java
+++ src/main/java/com/app/job/jobKorea/service/JobKoreaResumeUpdaterService.java
@@ -1,42 +1,37 @@
 package com.app.job.jobKorea.service;
 
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Component;
 
 import com.app.job.dto.req.MemberReqDTO;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
-import java.time.Duration;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class JobKoreaResumeUpdaterService {
+	
+	private final WebDriver driver;
+	
     public void updateResume(MemberReqDTO memberReqDTO) throws Exception {
 //        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Google\\Chrome\\Application\\chromedriver.exe");
 
-    	ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless"); 				                //GUI 없이 실행 (필수)
-        options.addArguments("--no-sandbox"); 				                //보안 정책 우회 (메모리 절약)
-        options.addArguments("--disable-dev-shm-usage"); 	                //공유 메모리 비활성화 (Heroku 필수)
-        options.addArguments("--disable-gpu"); 				                //GPU 사용 비활성화
-        options.addArguments("--remote-allow-origins=*"); 	                //원격 실행 허용
-        options.addArguments("--disable-background-timer-throttling"); 		//백그라운드 작업 최소화
-        options.addArguments("--disable-backgrounding-occluded-windows"); 	//백그라운드 윈도우 최소화
-        options.addArguments("--disable-extensions"); 						//확장 프로그램 비활성화
-
-        WebDriver driver = new ChromeDriver(options);
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebDriverWait wait = null;
 
         try {
             driver.get("https://www.jobkorea.co.kr/Login/Login_Tot.asp");
             log.info("✅ JobKorea 홈페이지 접속 완료");
 
             // 로그인
+            wait = new WebDriverWait(driver, Duration.ofSeconds(10));
             WebElement id = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("M_ID")));
             WebElement pw = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("M_PWD")));
             WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(By.className("login-button")));
@@ -46,7 +41,7 @@ public class JobKoreaResumeUpdaterService {
             loginButton.click();
 
             // 로그인 성공 여부 확인
-//            Thread.sleep(3000); // 로그인 요청 후 일정 시간 대기 (네트워크 지연 대응)
+//            Thread.sleep(3000); // 로그인 요청 후 일정 시간 대기 (네트워크 지연 대응) 로컬용
 
             WebElement linkElement = driver.findElement(By.cssSelector(".status a"));
             driver.get(linkElement.getAttribute("href"));
